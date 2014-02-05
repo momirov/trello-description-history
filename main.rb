7f8c765094c8fadd7474d9174d7579b6edcd9224
Jeff Weiss <jeff.weiss@puppetlabs.com>
2014-02-05 14:22:06 -0800

    add comments about what's going on

--- main.rb
+++ main.rb
@@ -5,6 +5,13 @@ require 'uri'
 
 use Rack::Session::Pool
 
+# Let's ensure that we have a client just for us in the session before we
+# process any URIs. It's cool tho, we'll save what you were requesting and send
+# you back there after you've been authorized.
+#
+# The current version of the ruby-trello gem doesn't allow us to set the name
+# of our application so we'll append it to the authorization URL, encoding it
+# so that we can have things like spaces or slashes.
 before do
   if session[:client].nil?
     session[:client] = Trello::Client.new( 
@@ -18,14 +25,24 @@ before do
   end
 end
 
+# Show me a list of the boards that I have access to
+#
+# This alone, is relatively useless from a user's perspective, but it's the
+# groundwork for development of custom reports/workflow/etc.
 get '/' do
   me = session[:client].find(:member, 'me')
   erb :index, :locals => { :me => me, :boards => me.boards}
 end
 
+# Create an access token and store it in our client object.
 get '/oauth/callback' do
   rt = session[:request_token]
+
+  # this :oauth_verifier parameter was missing from nearly *every* example
+  # that I found, but is completely necessary. You're welcome.
   at = rt.get_access_token :oauth_verifier => params[:oauth_verifier]
   session[:client].auth_policy.token = Trello::Authorization::OAuthCredential.new at.token, nil
+
+  # Return from whence you came - http://www.youtube.com/watch?v=qBW2z0NZ5zA
   redirect URI::decode(params[:return])
 end
